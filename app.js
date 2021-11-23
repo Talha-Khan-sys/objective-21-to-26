ba1a5a5c8ee06981f17586c562ae5fe49a9b80bf
Muhammad Talha Khan <hdmoviez1234@gmail.com>
2021-11-23 08:42:03 +0500

    objective 21 to 26

---
+++ app.js
@@ -0,0 +1,70 @@
+                           ////////          ASSIGNMENT    Chapter 21 to 26    ////////
+// var fname = prompt("Enter Your First Name");
+// var lname = prompt("Enter Your List Name");
+// alert("Your Full Name is " + " " + fname+lname);
+
+
+// var phone =prompt("Enter Your Favourite Phone");
+// console.log("Your Favourite Phone is" + ":" + phone);
+// var plength = phone.length;
+// for(i=0; i<plength; i++){
+//     console.log("Length of String is" + ":" + plength)
+// };
+
+
+// var a = "Pakistani";
+// console.log("String" + ":" + a)
+// var aindex =a.indexOf("n");
+// console.log("Index of 'n'" + ":" + aindex);
+
+
+
+// var a = "Hello World";
+// console.log("String" + ":" + a)
+// var aindex =a.lastIndexOf("l");
+// console.log("Last index of 'l'" + ":" + aindex);
+
+
+// var a = "Pakistani";
+// console.log("String" + ":" + a);
+// var charaindex = a.charAt(3);
+// console.log("Character at index"+ " " + 3 + ":" + " " + charaindex);
+
+
+// var fname = prompt("Enter Your First Name");
+// var lname = prompt("Enter Your List Name");
+// alert("Your Full Name is " + " " + fname+lname);
+
+
+// var a = "Hyderabad";
+// console.log("City" + ": " + a);
+// var replaceA = a.replace("Hyder", "Islam");
+// console.log("After Replacemnt" + ": " + replaceA);
+
+
+// var message = "Ali and Sami are best friends. They Play cricket and football together."
+// console.log(message);
+// var repmessage = message.replace(/and/g, "&");
+// console.log("After Replacemnt" + ": " + repmessage);
+
+
+// var user =prompt("Enter Your Text");
+// console.log("User Input" + ": " + user)
+// var toUpperCase =user.toUpperCase();
+// console.log("Upper Case" + ": " + toUpperCase);
+
+
+// var uinput =prompt("Enter your text");
+// console.log("User Input" + ": " + uinput);
+// var copy =uinput.slice(0,1);
+// var copy1 =uinput.slice(1);
+// copy =copy.toUpperCase();
+// copy1 =copy1.toLowerCase();
+// var titlecase =copy + copy1;
+// console.log("Title Case" + ": " + titlecase); 
+
+
+
+// var num =35.36;
+// var round =Math.round(num);
+// console.log(round);
